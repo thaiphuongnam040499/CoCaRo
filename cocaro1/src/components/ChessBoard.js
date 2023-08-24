87b2a514f905b7d8d011b26e5ed0f5f73740ef5d
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-08-24 14:26:19 +0700

    up2

---
+++ cocaro1/src/components/ChessBoard.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { findAllRoom } from "../redux/reducer/roomSlice";
+import socketIOClient from "socket.io-client";
+
+const host = "http://localhost:4002";
+const ROWS = 16;
+const COLS = 16;
+const WIN_LENGTH = 5;
+
+export default function ChessBoard() {
+  const [board, setBoard] = useState(Array(ROWS).fill(Array(COLS).fill(null)));
+  const [xIsNext, setXIsNext] = useState(true);
+  const userLogin = JSON.parse(localStorage.getItem("userLogin"));
+  const rooms = useSelector((state) => state.room.listRoom);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(findAllRoom());
+  }, []);
+
+  const socketRef = useRef();
+  const messagesEnd = useRef();
+
+  useEffect(() => {
+    socketRef.current = socketIOClient.connect(host);
+
+    socketRef.current.on("sendDataServer", (dataGot) => {
+      setBoard(dataGot.data);
+    });
+
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  const handleClick = (row, col) => {
+    if (board[row][col] || calculateWinner(board)) {
+      return;
+    }
+    const newBoard = board.map((r, i) =>
+      i === row ? r.map((c, j) => (j === col ? (xIsNext ? "X" : "O") : c)) : r
+    );
+    socketRef.current.emit("sendDataClient", newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (row, col) => (
+    <button className="square" onClick={() => handleClick(row, col)}>
+      <span
+        style={{
+          color: board[row][col] === "X" ? "black" : "red",
+        }}
+      >
+        {board[row][col]}
+      </span>
+    </button>
+  );
+
+  const renderBoard = () =>
+    board.map((row, rowIndex) => (
+      <div key={rowIndex} className="board-row">
+        {row.map((_, colIndex) => renderSquare(rowIndex, colIndex))}
+      </div>
+    ));
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+  return (
+    <div className="d-flex justify-content-center align-items-center mt-5">
+      <div className="game">
+        <div className="game-info">
+          <div className="text-light text-center">{status}</div>
+        </div>
+        <div className="game-board">{renderBoard()}</div>
+      </div>
+    </div>
+  );
+}
+function calculateWinner(board) {
+  const directions = [
+    [0, 1], // check hang ngang
+    [1, 0], // check hang doc
+    [1, 1], // duong cheo trai xuong phai
+    [-1, 1], // duong cheo phai xuong trai
+  ];
+
+  const checkWin = (row, col, dirX, dirY) => {
+    const player = board[row][col];
+    if (!player) return false;
+
+    for (let i = 1; i < WIN_LENGTH; i++) {
+      const newRow = row + dirY * i;
+      const newCol = col + dirX * i;
+      if (
+        newRow < 0 ||
+        newRow >= ROWS ||
+        newCol < 0 ||
+        newCol >= COLS ||
+        board[newRow][newCol] !== player
+      ) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLS; col++) {
+      for (const [dirX, dirY] of directions) {
+        if (checkWin(row, col, dirX, dirY)) {
+          return board[row][col];
+        }
+      }
+    }
+  }
+
+  return null;
+}
--- cocaro1/src/components/Room.js
+++ cocaro1/src/components/Room.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import Board from "./Board";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteRoom, findAllRoom } from "../redux/reducer/roomSlice";
 import { useNavigate } from "react-router-dom";
+import "../../src/assets/board.css";
+import ChessBoard from "./ChessBoard";
 
 export default function Room() {
-  const [squares, setSquares] = useState(Array(64).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
   const userLogin = JSON.parse(localStorage.getItem("userLogin"));
   const dispatch = useDispatch();
   const rooms = useSelector((state) => state.room.listRoom);
@@ -16,24 +15,6 @@ export default function Room() {
   useEffect(() => {
     dispatch(findAllRoom());
   }, []);
-
-  const handleClick = (i) => {
-    if (checkWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = xIsNext ? "X" : "O";
-    setSquares(squares);
-    setXIsNext(!xIsNext);
-  };
-
-  const winner = checkWinner(squares);
-
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
 
   const handleOutRoom = () => {
     if (roomFind && userLogin) {
@@ -68,49 +49,21 @@ export default function Room() {
           />
         </div>
       </div>
-      <div className="d-flex justify-content-center align-items-center mt-5">
-        <div className="game">
-          <div className="game-board">
-            <div className="game-info mb-3">
-              <div className="text-light">{status}</div>
-            </div>
-            <Board squares={squares} onClick={(i) => handleClick(i)} />
-          </div>
-        </div>
-      </div>
+      <ChessBoard />
       <div className="chat-box bg-light ms-3 p-3">
         <p className="fs-6">Chat</p>
         <div className="border rounded p-2 chat-box-item">
           <div>
             <p className="fs-6">abcde</p>
           </div>
-          <div className="d-flex send-mess">
-            <input type="text" className="border rounded me-2 w-100" />
-            <div className="align-items-center">
-              <button className="btn btn-success">send</button>
-            </div>
+        </div>
+        <div className="d-flex send-mess mt-2">
+          <input type="text" className="border rounded me-2 w-100" />
+          <div className="align-items-center">
+            <button className="btn btn-success">send</button>
           </div>
         </div>
       </div>
     </div>
   );
-}
-function checkWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
